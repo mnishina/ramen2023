6ff913e465ba9afc6dfaa2576bdfe66a39dc7ed9
M Nishina <mitsunorinishina@icloud.com>
2024-01-17 23:43:30 +0900

    fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import { glslify } from "vite-plugin-glslify";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [glslify()],
+    plugins: [glslify()]
   },
+  output: "server",
+  adapter: vercel()
 });
